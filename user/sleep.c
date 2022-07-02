f42e6bfbdab283e88cb3c718d95895e2ca5bd924
Rahul Thomas <rapidclock@users.noreply.github.com>
2022-07-02 00:31:14 -0700

    sleep and some stubs

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int sleep_ticks;
+    if (argc != 2)
+    {
+        fprintf(2, "Usage : sleep ticks\n");
+        exit(1);
+    }
+    sleep_ticks = atoi(argv[1]);
+    sleep(sleep_ticks);
+    exit(0);
+}

0ff6964e935d31f532290297aea6d62821160e8b
Rahul Thomas <rapidclock@users.noreply.github.com>
2022-07-15 02:31:12 -0700

    xargs - 03

--- user/xargs.c
+++ user/xargs.c
@@ -20,11 +20,11 @@
 // }
 
 void subcommand(char *argv[], int argc) {
-  int i = 0;
+  // int i = 0;
   if (fork() == 0) {
-    for (i = 0; i < argc; i++) {
-      printf("[Debug] Inside sub-fn, args [%d] : %s\n", i, argv[i]);
-    }
+    // for (i = 0; i < argc; i++) {
+    //   printf("[Debug] Inside sub-fn, args [%d] : %s\n", i, argv[i]);
+    // }
     exec(argv[0], argv);
   } else {
     wait((int *)0);
@@ -46,17 +46,11 @@ int main(int argc, char *argv[]) {
     args[i - 1] = argv[i];
   }
 
-  for (i = 0; i < argc - 1; i++) {
-    printf("[Debug] args [%d] : %s\n", i, args[i]);
-  }
-
   // read char by char and process the args at the same time.
   // terminate every space with a /0 and break to the subshell
   // when we encounter a /n (newline).
   while (read(0, cur, 1) > 0) {
     d_cnt++;
-    //    printf("[Debug] Count : %d\n", d_cnt);
-    //    printf("[Debug] sub_argc : %d\n", sub_argc);
     switch (*cur) {
       case ' ':
         *cur = '\0';
@@ -67,7 +61,10 @@ int main(int argc, char *argv[]) {
         break;
       case '\n':
         *cur = '\0';
+        args[sub_argc] = word;
+        sub_argc++;
         subcommand(args, sub_argc);
+        cur++;
         sub_argc = argc - 1;  // reset to initial value for next call.
         break;
       case '\0':

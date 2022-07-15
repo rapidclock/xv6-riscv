0d9a9bd1037b5596a5735f2098cbb0e8907f0587
Rahul Thomas <rapidclock@users.noreply.github.com>
2022-07-15 00:37:25 -0700

    xargs - 02

--- user/xargs.c
+++ user/xargs.c
@@ -4,19 +4,39 @@
 
 #define BUFFER_SIZE 256
 
-void subcommand(char *argv[]) {
+// void subcommand(char buf[], char *argv[], int argc) {
+//   int i = 0;
+//   if (fork() == 0) {
+//     printf("Buffer Contents : %s\n", buf);
+//
+//     for (i = 0; i < argc; i++) {
+//       printf("[Debug] Inside sub-fn, args [%d] : %s\n", i, argv[i]);
+//     }
+//
+//     exec(argv[0], argv);
+//   } else {
+//     wait((int*) 0);
+//   }
+// }
+
+void subcommand(char *argv[], int argc) {
+  int i = 0;
   if (fork() == 0) {
+    for (i = 0; i < argc; i++) {
+      printf("[Debug] Inside sub-fn, args [%d] : %s\n", i, argv[i]);
+    }
     exec(argv[0], argv);
   } else {
-    wait((int*) 0);
+    wait((int *)0);
   }
 }
 
 int main(int argc, char *argv[]) {
-  char *args[MAXARG];
+  char *args[MAXARG] = {0};
   char *cur, *word;
   char inp_buf[BUFFER_SIZE];
   int i, sub_argc;
+  int d_cnt = 0;
 
   cur = word = inp_buf;
   sub_argc = argc - 1;
@@ -26,10 +46,17 @@ int main(int argc, char *argv[]) {
     args[i - 1] = argv[i];
   }
 
+  for (i = 0; i < argc - 1; i++) {
+    printf("[Debug] args [%d] : %s\n", i, args[i]);
+  }
+
   // read char by char and process the args at the same time.
   // terminate every space with a /0 and break to the subshell
   // when we encounter a /n (newline).
-  while (read(0, &cur, 1) > 0) {
+  while (read(0, cur, 1) > 0) {
+    d_cnt++;
+    //    printf("[Debug] Count : %d\n", d_cnt);
+    //    printf("[Debug] sub_argc : %d\n", sub_argc);
     switch (*cur) {
       case ' ':
         *cur = '\0';
@@ -40,13 +67,14 @@ int main(int argc, char *argv[]) {
         break;
       case '\n':
         *cur = '\0';
-        subcommand(args);
-        sub_argc = argc - 1; // reset to initial value for next call.
+        subcommand(args, sub_argc);
+        sub_argc = argc - 1;  // reset to initial value for next call.
         break;
       case '\0':
-        subcommand(args);
+        subcommand(args, sub_argc);
         break;
       default:
+        cur++;
         break;
     }
   }
